7ca2960ce5880ba1677103bbd8bf156586ec2dce
王嘉伟 <wanglihuaya@outlook.com>
2023-09-07 02:28:20 +0800

    feat: add icon render

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,21 +1,57 @@
-import { Button, Grid } from "@nutui/nutui-react-taro";
-import { View } from "@tarojs/components";
+import { Button, Grid, Overlay } from "@nutui/nutui-react-taro";
+import { Image, View } from "@tarojs/components";
 import { useEnv } from "taro-hooks";
 
-import { getTestData } from "@/services";
+import { getTestData, getTestData1, getTestData2 } from "@/services";
 import { useEffect, useState } from "react";
 import "./index.less";
 
 function Index() {
   const env = useEnv();
   const [cssData, setCssData] = useState({} as any);
+  const [iconList, setIconList] = useState([] as any);
+  const [visible, setVisible] = useState(false);
+  const [selectIcon, setSelectIcon] = useState("");
 
   const init = async () => {
+    const nowIconList: any = [];
     try {
       const res = await getTestData();
+      // 所有的类比的名字
+      const res1 = await getTestData1();
+      // 获取 res1 所有的 key
+      const setList = Object.keys(res1).slice(0, 5);
+      setList.forEach(async (item) => {
+        // 每一个类别的所有的 icon的名字
+        const res2: any = await getTestData2(item);
+        if (res2) {
+          const list: any = [];
+          if (res2?.uncategorized?.length > 0) {
+            list.push(...res2?.uncategorized.flat());
+          }
+          if (res2?.hidden?.length > 0) {
+            list.push(...res2?.hidden.flat());
+          }
+          if (res2?.categories && Object.values(res2?.categories)?.length > 0) {
+            const _list = Object.values(res2?.categories)?.map((_item) => {
+              return _item;
+            });
+            list.push(..._list.flat());
+          }
+
+          nowIconList.push({
+            name: item,
+            list: list,
+          });
+          console.log("nowIconList", nowIconList.length);
+          setIconList(nowIconList);
+        }
+      });
       const matches = res.match(/url\((.*)\);/)[1];
       setCssData(matches);
-    } catch (error) {}
+    } catch (error) {
+    } finally {
+    }
   };
 
   useEffect(() => {
@@ -27,7 +63,13 @@ function Index() {
       <View className="index">欢迎使用 NutUI React 开发 Taro 多端项目。</View>
       <View className="index">当前环境: {env}</View>
       <View className="index">
-        <Button type="primary" className="btn">
+        <Button
+          type="primary"
+          className="btn"
+          onClick={() => {
+            console.log("click", iconList.length);
+          }}
+        >
           NutUI React Button
         </Button>
       </View>
@@ -44,42 +86,54 @@ function Index() {
         className="w-[100px] h-[100px]"
       />
       {/* <Image src="https://api.iconify.design/fluent-emoji-flat/alarm-clock.svg" /> */}
-      <View className="op30 text-lg fw300 m1">
-        The instant on-demand Atomic CSS engine.
-      </View>
-      <View className="w-md h-md i-carbon-logo-github" />
+
       <Grid>
-        <Grid.Item>
-          <View className="w-[100px] h-[100px] i-logos-adobe-after-effects" />
-        </Grid.Item>
-        <Grid.Item>
-          <View className="w-[100px] h-[100px] i-logos-adobe-animate" />
-        </Grid.Item>
-        <Grid.Item>
-          <View className="w-[100px] h-[100px] i-logos-adobe-dreamweaver" />
-        </Grid.Item>
-        <Grid.Item>
-          <View className="w-[100px] h-[100px] i-logos-adobe-illustrator" />
-        </Grid.Item>
-        <Grid.Item>
-          <View className="w-[100px] h-[100px] i-logos-adobe-incopy" />
-        </Grid.Item>
-        <Grid.Item>
-          <View className="w-[100px] h-[100px] i-logos-adobe-indesign" />
-        </Grid.Item>
-        <Grid.Item>
-          <View className="w-[100px] h-[100px] i-logos-adobe-lightroom" />
-        </Grid.Item>
-        <Grid.Item>
-          <View className="w-[100px] h-[100px] i-logos-adobe-photoshop" />
-        </Grid.Item>
-        <Grid.Item>
-          <View className="w-[100px] h-[100px] i-logos-adobe-premiere" />
-        </Grid.Item>
-        <Grid.Item>
-          <View className="w-[100px] h-[100px] i-logos-adobe-xd" />
-        </Grid.Item>
+        {iconList.map((item: any, index) => {
+          const _list = item.list.slice(0, 500);
+          return _list.map((item1: any, index1) => {
+            return (
+              <Grid.Item
+                key={index1 + index}
+                onClick={() => {
+                  setVisible(true);
+                  setSelectIcon(
+                    `https://api.iconify.design/${item.name}/${item1}.svg`
+                  );
+                }}
+              >
+                <Image
+                  lazyLoad
+                  fadeIn
+                  showMenuByLongpress
+                  className="w-[70px] h-[70px]"
+                  src={`https://api.iconify.design/${item.name}/${item1}.svg`}
+                />
+                {/* <View className="w-[100px] h-[100px]">
+                    <View className={item1} />
+                  </View> */}
+              </Grid.Item>
+            );
+          });
+        })}
       </Grid>
+      <Overlay
+        visible={visible}
+        onClick={() => {
+          setVisible(false);
+        }}
+        className="center"
+      >
+        <Image
+          onClick={() => {
+            setVisible(false);
+          }}
+          lazyLoad
+          fadeIn
+          showMenuByLongpress
+          className="  w-[500px] h-[500px]"
+          src={selectIcon}
+        />
+      </Overlay>
     </View>
   );
 }
--- src/services/index.tsx
+++ src/services/index.tsx
@@ -6,3 +6,15 @@ export async function getTestData() {
     url: "https://api.iconify.design/openmoji.css?icons=apple",
   });
 }
+export async function getTestData1() {
+  return request.get({
+    url: "https://api.iconify.design/collections",
+  });
+}
+
+export async function getTestData2(item: string) {
+  return request.get({
+    // url: `https://api.iconify.design/collections?prefixes=${item}&pretty=1`,
+    url: `https://api.iconify.design/collection?prefix=${item}&pretty=1`,
+  });
+}
